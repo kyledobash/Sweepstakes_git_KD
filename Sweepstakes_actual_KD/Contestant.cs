2c6ad5d2601d183ede08f3ea19914a4d80ec3bc2
kyledobash <kyledobash@yahoo.com>
2021-01-18 09:35:31 -0700

    Created the contestant class, and populated with member variables, also built constructor.

---
+++ Sweepstakes_actual_KD/Contestant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_actual_KD
+{
+    public class Contestant
+    {
+        // member variables
+        string firstName;
+        string lastName;
+        string address;
+        int registrationNumber;
+
+        // constructor
+        public Contestant(string firstname, string lastname, string address, int registrationnumber)
+        {
+            this.firstName = firstname;
+            this.lastName = lastname;
+            this.address = address;
+            this.registrationNumber = registrationnumber;
+        }
+        // member methods
+
+    }
+}
