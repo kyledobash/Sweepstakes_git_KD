4f8f63767fb879ef2adb7976c657f279dc7b01c0
kyledobash <kyledobash@yahoo.com>
2021-01-19 12:14:34 -0700

    Changed Simulation access modifier to public.

--- Sweepstakes_actual_KD/Simulation.cs
+++ Sweepstakes_actual_KD/Simulation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes_actual_KD
 {
-    class Simulation
+    public class Simulation
     {
         // member variables
         MarketingFirm marketingFirm;
